b083750e2a95098d0fa973cef5721744e407b1c0
Hamza Farooq <hamzafarooq109@gmail.com>
2026-07-23 18:12:49 +0500

    make todo app interface with sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,72 @@
-import Image from "next/image";
+"use client";
+
+import type { Viewport } from 'next'
+import { useState } from "react";
+
+// export const viewport: Viewport = {
+//   themeColor: 'black',
+// }
 
 export default function Home() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  function toggleSideBar() {
+    setIsSidebarOpen((prevState) => !prevState);
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="container flex bg-[#F3F4F6]">
+      <aside
+        className={`sidebar bg-[#FFFFFF] h-screen flex flex-col transition-all duration-300 overflow-hidden  ${isSidebarOpen ? "w-60" : "w-20"} `}
+      >
+        <img src="/menu.svg" alt="" onClick={toggleSideBar} className= {`menu ${isSidebarOpen? "pl-4 pt-3 w-13" : "px-4 py-3 w-13 min-w-15"} `} />
+        { isSidebarOpen && (<div className="flex flex-col justify-center items-center mx-6 mb-3">
+          <img src="/avatar.png" className="m-2 w-20"/>
+          <h1 className="text-black self-center">Jane Doe</h1>
+          <p className="text-[#6A7282] self-center" >janedoe@gmail.com</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+        )}
+        <hr className={`w-[80%] border-t border-gray-500 ${isSidebarOpen? "self-center":""}`} />
+        <div className="flex items-center">
+        <img src="/list-todo.svg" alt="" className="px-4 py-3 w-13 min-w-15" />
+        { isSidebarOpen && (<h1 className="text-black font-bold">My Tasks</h1>)}
+        </div>
+
+        <div className="flex items-center">
+        <img src="/settings.svg" alt="" className=" px-4 py-3 w-13 min-w-15" />
+        { isSidebarOpen && (<h1 className="text-black font-bold">Settings</h1>)}
+        </div>
+
+      </aside>
+
+      <main className="w-full flex flex-col gap-4">
+        <img
+          src="/moon.svg"
+          alt=""
+          className="self-end p-3 max-w-20 w-13 block"
+        />
+
+        <strong className="text-black self-center text-4xl font-sans">
+          My Tasks
+        </strong>
+        <div className="flex gap-4 justify-center">
+          <input
+            type="text"
+            autoFocus
+            placeholder="Type your task here.."
+            className="bg-[#FFFFFF] placeholder-[#A9A9A9] rounded-2xl w-[20%] pl-4 pr-2 py-2 text-black"
+          />
+          <button
+            type="submit"
+            className="h-auto w-auto text-white bg-black rounded-2xl px-7 py-2"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            Add
+          </button>
+        </div>
+
+        <div className="self-center mt-auto p-4">
+          <span className="w-10 text-black">&copy; 2025</span>
         </div>
       </main>
     </div>
